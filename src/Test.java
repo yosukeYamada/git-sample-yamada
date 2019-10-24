08c4146bd18c4d1dc0ac91f8eebf1006cc0ab9bd
yosukeYamada <yamada_141@yahoo.co.jp>
2019-10-24 13:51:17 +0900

    getter setterを追加

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,18 @@ public class Test {
 
 	private int count;
 	private String name;
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
